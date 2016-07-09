01d7b48c8589245e63780155da2512fbc0e94747
alinemorelli <alinemorelliw@gmail.com>
2016-07-09 09:50:06 -0300

    ajustes

---
+++ wordpress/wp-content/themes/codethemachine/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="icon" type="image/png" href="wp-content/themes/codethetheme/img/favicon.png" />	
+	<link rel="stylesheet" href="wp-content/themes/codethetheme/style.css">
+	<link rel="icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico" type="image/x-icon" />
+	<title>Code The Machine</title>
+</head>
+<body>
+	<main class="main">
+		<header>
+			<div class="container">
+				<div class="headerContent">
+					<h1>
+						<a href="#"><img src="wp-content/themes/codethetheme/img/codethemachine.svg" width="55" height="55" alt="Code The Machine"/>
+							CodeTheMachine
+						</a>
+					</h1>
+					<nav>		
+						<ul class="menu">
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Tutoriais</a></li>
+							<li><a href="#">Novidades</a></li>
+							<li><a href="#">Portfólio</a></li>
+						</ul>
+					</nav>
+					<button class="menuAbrir">
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+					<a class="search" href="#"></a>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+			<section class="content">
+				<ul class="categories">
+					<li class="nomeCategoria">
+						<a href="" class="tituloBox"><span class="titulo">nome1</span></a>
+					</li>
+					<li class="nomeCategoria">
+						<a href="" class="tituloBox"><span class="titulo">nome1</span></a>
+					</li>
+					<li class="nomeCategoria">
+						<a href="" class="tituloBox"><span class="titulo">nome1</span></a>
+					</li>
+				</ul>
+				<ul class="featured">
+					<li class="postFeatured">
+						<a href="" class="tituloBox"><span class="titulo">nome1</span></a>
+					</li>
+					<li class="postFeatured">
+						<a href="" class="tituloBox"><span class="titulo">nome1</span></a>
+					</li>
+				</ul>
+				<ul class="recents">
+					<li class="postRecents">
+						<a href="" class="tituloBox"><span class="titulo">nome1</span></a>
+					</li>
+					<li class="postRecents">
+						<a href="" class="tituloBox"><span class="titulo">nome1</span></a>
+					</li>
+					<li class="postRecents">
+						<a href="" class="tituloBox"><span class="titulo">nome1</span></a>
+					</li>
+				</ul>
+			</section>
+			<footer>
+				<ul class="siteMap">
+					<li><a href="#">Home</a></li>
+					<li><a href="">Tutoriais</a></li>
+					<li><a href="">Novidades</a></li>
+					<li><a href="">Portfólio</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+				<hr/>
+				<span>Codethemachine • 2016</span> 
+			</footer>
+		</div>
+	</main>
+	<script src="wp-content/themes/codethetheme/js/scripts.js"></script>
+</body>
+</html>
